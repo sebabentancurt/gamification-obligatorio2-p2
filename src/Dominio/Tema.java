6f58f0d7997fc867b86df8ce38fa2280726d3d9e
Mateo <mateo.sapiurka@gmail.com>
2020-06-10 19:48:19 -0300

    Dominio: Clase Tema

---
+++ src/Dominio/Tema.java
@@ -0,0 +1,52 @@
+package Dominio;
+
+import java.util.HashMap;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Mateo
+ */
+public class Tema {
+
+    private String nombre;
+    private String descripcion;
+    private HashMap<String, String> preguntas;
+
+    //constructor sin parametros
+    public Tema() {
+        this.setNombre("sin nombre");
+        this.setDescripcion("sin descripcion");
+        preguntas = new HashMap<String, String>();
+    }
+    
+    //constructor con parametros
+    public Tema(String unNombre, String unaDescripcion) {
+        this.setNombre(unNombre);
+        this.setDescripcion(unaDescripcion);
+        preguntas = new HashMap<String, String>();
+    }
+
+    //setters
+    public void setNombre(String unNombre) {
+        this.nombre = unNombre;
+    }
+
+    public void setDescripcion(String unaDescripcion) {
+        this.descripcion = unaDescripcion;
+    }
+
+    //getters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+}
