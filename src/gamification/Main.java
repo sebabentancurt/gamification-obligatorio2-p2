2e4655a556ecb87e75e7257a7db541676efd2e5d
Sebastian Bentancurt <sebabentancurt@gmail.com>
2020-06-10 01:38:42 -0300

    Dashboard

--- src/gamification/Main.java
+++ src/gamification/Main.java
@@ -5,6 +5,8 @@
  */
 package gamification;
 
+import gamification.views.Dashboard;
+
 /**
  *
  * @author sebab
@@ -15,7 +17,10 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        try {
+            new Dashboard().setVisible(true);
+        } catch (Exception e) {
+        }
     }
     
 }
