bf7ed75de704335039a598dc8c68c0966f98abf6
Mateo <mateo.sapiurka@gmail.com>
2020-06-10 19:58:40 -0300

    Dominio: Clase Sistema

---
+++ src/Dominio/Sistema.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Dominio;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Mateo
+ */
+public class Sistema {
+
+    private ArrayList<Tema> listaTemas;
+
+    public Sistema() {
+        listaTemas = new ArrayList<Tema>();
+    }
+
+    //getters
+    public ArrayList<Tema> getListaTemas() {
+        return listaTemas;
+    }
+    
+    public void agregarTema(Tema unTema){
+        this.getListaTemas().add(unTema);
+    }
+
+}
